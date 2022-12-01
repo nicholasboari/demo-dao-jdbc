3eaf3fa31e0642f20faaefe66c56a3e3645096a1
nicholasboari <nicholasboari@gmail.com>
2022-12-01 09:32:05 -0300

    Seller entity class

--- src/main/java/com/nicholasboari/app/Program.java
+++ src/main/java/com/nicholasboari/app/Program.java
@@ -1,15 +1,18 @@
 package com.nicholasboari.app;
 
 import java.sql.Connection;
+import java.util.Date;
 
 import com.nicholasboari.db.DbConnection;
 import com.nicholasboari.model.entities.Department;
+import com.nicholasboari.model.entities.Seller;
 
 public class Program {
   public static void main(String[] args) {
 
     Connection conn = DbConnection.getConnection();
     Department obj = new Department(1, "Books");
-    System.out.println(obj);
+    Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+    System.out.println(seller);
   }
 }
