d0415986052685a9e99301dccc3ff448f78c85d5
nicholasboari <nicholasboari@gmail.com>
2022-12-01 12:09:10 -0300

    findById implementation

--- src/main/java/com/nicholasboari/app/Program.java
+++ src/main/java/com/nicholasboari/app/Program.java
@@ -1,21 +1,16 @@
 package com.nicholasboari.app;
 
-import java.sql.Connection;
-import java.util.Date;
-
-import com.nicholasboari.db.DbConnection;
 import com.nicholasboari.model.dao.DaoFactory;
 import com.nicholasboari.model.dao.SellerDao;
-import com.nicholasboari.model.entities.Department;
 import com.nicholasboari.model.entities.Seller;
 
 public class Program {
   public static void main(String[] args) {
 
-    Connection conn = DbConnection.getConnection();
-    Department obj = new Department(1, "Books");
-    Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
     SellerDao sellerDao = DaoFactory.creatSellerDao();
+
+    Seller seller = sellerDao.findById(5);
+
     System.out.println(seller);
   }
 }
--- src/main/java/com/nicholasboari/model/dao/DaoFactory.java
+++ src/main/java/com/nicholasboari/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package com.nicholasboari.model.dao;
 
+import com.nicholasboari.db.DbConnection;
 import com.nicholasboari.model.dao.impl.DepartmenteDaoJDBC;
 import com.nicholasboari.model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
   public static SellerDao creatSellerDao() {
-    return new SellerDaoJDBC();
+    return new SellerDaoJDBC(DbConnection.getConnection());
   }
 
   public static DepartmentDao creatDepartmentDao() {
