a623c0974b292df4978c07f8a97edb4fa97e9a56
nicholasboari <nicholasboari@gmail.com>
2022-12-02 19:05:38 -0300

    update implementation

--- src/main/java/com/nicholasboari/app/Program.java
+++ src/main/java/com/nicholasboari/app/Program.java
@@ -30,10 +30,15 @@ public class Program {
       System.out.println(s);
     }
 
-    System.out.println("\n=== TEST 5: seller insert ===");
-    Seller newSeller = new Seller(null, "Matheus Boari", "matheus@boari", new Date(), 8000.0, department);
-    System.out.println(newSeller.getId());
+    System.out.println("\n=== TEST 4: seller insert ===");
+    Seller newSeller = new Seller(null, "Nicholas Boari", "nicholas@boari", new Date(), 8000.0, department);
     sellerDao.insert(newSeller);
     System.out.println("Inserted! New id = " + newSeller.getId());
+
+    System.out.println("\n=== TEST 5: seller insert ===");
+    seller = sellerDao.findById(1);
+    seller.setName("Nicholas Boari");
+    sellerDao.update(seller);
+    System.out.println("Updated!");
   }
 }
